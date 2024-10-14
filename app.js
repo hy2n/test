953f16ed444b906a65af8f95de099bacf4f8e18a
현 <92370007+hy2n@users.noreply.github.com>
2024-10-14 19:34:13 +0900

    Update app.js

--- app.js
+++ app.js
@@ -4,14 +4,7 @@ const app = express();
 const port = 3000;
 
 app.get('/ip', async (req, res) => {
-  try {
-    const response = await axios.get('https://api.ipify.org?format=json');
-    const publicIp = response.data.ip;
-    res.json({ ip: publicIp });
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Error retrieving public IP');
-  }
+  res.status(500).send('Error retrieving public IP');
 });
 
 app.listen(port, () => {
